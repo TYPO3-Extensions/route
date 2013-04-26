bf12d2907cb2e82750060f8729cc4c7cfe2b895f
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-04-26 21:55:59 +0000

    0.0.1 +++ Initial Release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/route/trunk@74811 735d13b6-9817-0410-8766-e36946ffe9aa

--- tca.php
+++ tca.php
@@ -7,12 +7,253 @@ if (!defined ('TYPO3_MODE'))  die ('Access denied.');
   // 
   // INDEX
   // 
+  // Other wizards and config drafts
   // tx_route_category
   // tx_route_path
 
 
 
-  ///////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // 
+  // Other wizards and config drafts
+
+  $bool_exclude_default = false;
+
+  $arr_wizard_url = array (
+    'type'      => 'input',
+    'size'      => '80',
+    'max'       => '256',
+    'checkbox'  => '',
+    'eval'      => 'trim',
+    'wizards'   => array (
+      '_PADDING'  => '2',
+      'link'      => array (
+        'type'         => 'popup',
+        'title'        => 'Link',
+        'icon'         => 'link_popup.gif',
+        'script'       => 'browse_links.php?mode=wizard',
+        'JSopenParams' => $JSopenParams,
+      ),
+    ),
+    'softref' => 'typolink',
+  );
+
+  $conf_datetime = array (
+    'type'    => 'input',
+    'size'    => '10',
+    'max'     => '20',
+    'eval'    => 'datetime',
+    'default' => mktime(date('H'),date('i'),0,date('m'),date('d'),date('Y')),
+  );
+  
+  $conf_datetimeend = $conf_datetime;
+  unset($conf_datetimeend['default']);
+
+  $conf_archivedate = $conf_datetimeend;
+  $conf_archivedate['eval'] = 'date';
+
+  $conf_file_document = array (
+    'type'          => 'group',
+    'internal_type' => 'file',
+    'allowed'       => '',
+    'disallowed'    => 'php,php3', 
+    'max_size'      => $GLOBALS['TYPO3_CONF_VARS']['BE']['maxFileSize'], 
+    'uploadfolder'  => 'uploads/tx_org',
+    'show_thumbs'   => 1,
+    'size'          => 10,
+    'minitems'      => 0,
+    'maxitems'      => 99,
+  );
+
+  $conf_file_one_document             = $conf_file_document;
+  $conf_file_one_document['size']     = 1;
+  $conf_file_one_document['maxitems'] = 1;
+
+  $conf_file_image = array (
+    'type'          => 'group',
+    'internal_type' => 'file',
+    'allowed'       => $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'],
+    'max_size'      => $GLOBALS['TYPO3_CONF_VARS']['BE']['maxFileSize'],
+    'uploadfolder'  => 'uploads/tx_org',
+    'show_thumbs'   => 1,
+    'size'          => 3,
+    'minitems'      => 0,
+    'maxitems'      => 20,
+  );
+
+  $conf_file_one_image             = $conf_file_image;
+  $conf_file_one_image['size']     =  1;
+  $conf_file_one_image['maxitems'] = 99;
+
+  $conf_file_icon = array (
+    'type'          => 'group',
+    'internal_type' => 'file',
+    'allowed'       => $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'],
+    'max_size'      => $GLOBALS['TYPO3_CONF_VARS']['BE']['maxFileSize'],
+    'uploadfolder'  => 'uploads/tx_org',
+    'show_thumbs'   => 1,
+    'size'          => 1,
+    'minitems'      => 0,
+    'maxitems'      => 1,
+  );
+
+  $conf_input_30_trim = array (
+    'type' => 'input',
+    'size' => '30',
+    'eval' => 'trim'
+  );
+
+  $conf_input_30 = array (
+    'type' => 'input',
+    'size' => '30',
+  );
+
+  $conf_input_30_trimRequired = array (
+    'type' => 'input',
+    'size' => '30',
+    'eval' => 'trim,required'
+  );
+  
+  $conf_input_80_trim = array (
+    'type' => 'input',
+    'size' => '80',
+    'eval' => 'trim'
+  );
+  $conf_text_30_05 = array (
+    'type' => 'text',
+    'cols' => '30', 
+    'rows' => '5',
+  );
+  $conf_text_30_05_trimRequired = array (
+    'type' => 'text',
+    'cols' => '30', 
+    'rows' => '5',
+    'eval' => 'trim,required'
+  );
+  
+  $conf_text_50_10 = array (
+    'type' => 'text',
+    'cols' => '50', 
+    'rows' => '10',
+  );
+  
+  $conf_text_rte = array (
+    'type' => 'text',
+    'cols' => '30',
+    'rows' => '5',
+    'wizards' => array (
+      '_PADDING' => 2,
+      'RTE' => array (
+        'notNewRecords' => 1,
+        'RTEonly'       => 1,
+        'type'          => 'script',
+        'title'         => 'Full screen Rich Text Editing|Formatteret redigering i hele vinduet',
+        'icon'          => 'wizard_rte2.gif',
+        'script'        => 'wizard_rte.php',
+      ),
+    ),
+  );
+
+  $conf_hidden = array (
+    'exclude'   => $bool_exclude_default,
+    'l10n_mode' => 'exclude',
+    'label'     => 'LLL:EXT:lang/locallang_general.xml:LGL.hidden',
+    'config'    => array (
+      'type'    => 'check',
+      'default' => '0'
+    ),
+  );
+  $conf_starttime = array (
+    'exclude'   => $bool_time_control,
+    'l10n_mode' => 'exclude',
+    'label'     => 'LLL:EXT:lang/locallang_general.xml:LGL.starttime',
+    'config'    => array (
+      'type'     => 'input',
+      'size'     => '8',
+      'max'      => '20',
+      'eval'     => 'date',
+      'default'  => '0',
+      'checkbox' => '0'
+    ),
+  );
+  $conf_endtime = array (
+    'exclude'   => $bool_time_control,
+    'l10n_mode' => 'exclude',
+    'label'     => 'LLL:EXT:lang/locallang_general.xml:LGL.endtime',
+    'config'    => array (
+      'type'     => 'input',
+      'size'     => '8',
+      'max'      => '20',
+      'eval'     => 'date',
+      'checkbox' => '0',
+      'default'  => '0',
+      'range'    => array (
+        'upper' => mktime(0, 0, 0, date('m'), date('d'), date('Y')+30),
+        'lower' => mktime(0, 0, 0, date('m')-1, date('d'), date('Y')),
+      ),
+    ),
+  );
+  $conf_fegroup = array (
+    'exclude'     => $bool_fegroup_control,
+    //'l10n_mode'   => 'mergeIfNotBlank',
+    'label'       => 'LLL:EXT:lang/locallang_general.php:LGL.fe_group',
+    'config'      => array (
+      'type'      => 'select',
+      'size'      => $size_fegroup,
+      'maxitems'  => 20,
+      'items' => array (
+        array ('LLL:EXT:lang/locallang_general.php:LGL.hide_at_login', -1),
+        array ('LLL:EXT:lang/locallang_general.php:LGL.any_login', -2),
+        array ('LLL:EXT:lang/locallang_general.php:LGL.usergroups', '--div--'),
+      ),
+      'exclusiveKeys' => '-1,-2',
+      'foreign_table' => 'fe_groups'
+    ),
+  );
+  $conf_topnews = array (
+    'exclude' => $bool_exclude_none,
+//    'l10n_mode'   => 'exclude',
+    'label'   => 'LLL:EXT:org/locallang_db.xml:tx_org_news.topnews',
+    'config'  => array (
+      'type'    => 'check',
+      'default' => '0',
+    ),
+  );
+  if ($bool_topnews_sorting === true)
+  {
+    $conf_topnews['config'] = array (
+      'type'          => 'select',
+      'items' => array(
+        array('LLL:EXT:org/locallang_db.xml:tx_org_news.topnews.3', 3),
+        array('LLL:EXT:org/locallang_db.xml:tx_org_news.topnews.2', 2),
+        array('LLL:EXT:org/locallang_db.xml:tx_org_news.topnews.1', 1),
+        array('LLL:EXT:org/locallang_db.xml:tx_org_news.topnews.0', 0),
+      ),
+      'size'           => 4,
+      'maxitems'       => 1,
+      'suppress_icons' => 1,
+      'default'        => 0,
+    );
+  }
+  $conf_pages = array (
+    'exclude'   => $bool_exclude_default,
+    'l10n_mode' => 'exclude',
+    'label'     => 'LLL:EXT:org/locallang_db.xml:tca_phrase.pages',
+    'config'    => array (
+      'type'          => 'group',
+      'internal_type' => 'db',
+      'allowed'       => 'pages',
+      'size'          => '10',
+      'maxitems'      => '99',
+      'minitems'      => '0',
+      'show_thumbs'   => '1'
+    ),
+  );
+  // Other wizards and config drafts
+
+
+
+///////////////////////////////////////
   // 
   // tx_route_category
   
