7da296f5055791f2ea297e75b02d9759a1321f8d
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-04-28 17:03:38 +0000

    0.0.1 +++ Initial Release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/route/trunk@74883 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++ ext_tables.sql
@@ -142,6 +142,7 @@ CREATE TABLE tx_route_path_mm_tx_route_poi (
   uid_foreign int(11) DEFAULT '0' NOT NULL,
   tablenames varchar(30) DEFAULT '' NOT NULL,
   sorting int(11) DEFAULT '0' NOT NULL,
+  sorting_foreign int(11) unsigned DEFAULT '0' NOT NULL,
   KEY uid_local (uid_local),
   KEY uid_foreign (uid_foreign)
 );
