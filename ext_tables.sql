0e9430f61bdab796431b97b04294fa3ac8921afb
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-05-08 20:06:25 +0000

    0.0.1 +++ poi -> marker
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/route/trunk@75248 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,11 +2,11 @@
 # INDEX
 #
 # tx_route_category
+# tx_route_marker
+# tx_route_marker_mm_tx_route_category
 # tx_route_path
 # tx_route_path_mm_tx_route_category
 # tx_route_path_mm_tx_route_marker
-# tx_route_marker
-# tx_route_marker_mm_tx_route_category
 
 
 
@@ -36,6 +36,84 @@ CREATE TABLE tx_route_category (
 
   PRIMARY KEY (uid),
   KEY parent (pid)
+);
+
+
+
+#
+# Table structure for table 'tx_route_marker'
+#
+CREATE TABLE tx_route_marker (
+  uid int(11) NOT NULL auto_increment,
+  pid int(11) DEFAULT '0' NOT NULL,
+  tstamp int(11) DEFAULT '0' NOT NULL,
+  crdate int(11) DEFAULT '0' NOT NULL,
+  cruser_id int(11) DEFAULT '0' NOT NULL,
+  deleted tinyint(4) DEFAULT '0' NOT NULL,
+
+  sys_language_uid int(11) DEFAULT '0' NOT NULL,
+  l10n_parent int(11) DEFAULT '0' NOT NULL,
+  l10n_diffsource mediumtext,
+
+  title tinytext,
+  tx_route_category tinytext,
+  short tinytext,
+  bodytext mediumtext NOT NULL,
+
+  lat text NOT NULL,
+  lon text NOT NULL,
+  address text NOT NULL,
+  url text NOT NULL,
+
+  tx_route_path tinytext,
+
+  list_title tinytext,
+  list_short mediumtext,
+  map_title tinytext,
+  map_short mediumtext,
+
+  image text,
+  imagecaption text,
+  imageseo text,
+  imageheight tinytext,
+  imagewidth tinytext,
+  imageorient tinyint(4) unsigned NOT NULL default '0',
+  imagecaption text,
+  imagecols tinyint(4) unsigned NOT NULL default '0',
+  imageborder tinyint(4) unsigned NOT NULL default '0',
+  imagecaption_position varchar(12) default '',
+  image_link text,
+  image_zoom tinyint(3) unsigned NOT NULL default '0',
+  image_noRows tinyint(3) unsigned NOT NULL default '0',
+  image_effects tinyint(3) unsigned NOT NULL default '0',
+  image_compression tinyint(3) unsigned NOT NULL default '0',
+  image_frames tinyint(3) unsigned NOT NULL default '0',
+
+  hidden tinyint(4) DEFAULT '0' NOT NULL,
+  starttime int(11) DEFAULT '0' NOT NULL,
+  endtime int(11) DEFAULT '0' NOT NULL,
+  fe_group varchar(100) DEFAULT '0' NOT NULL,
+  
+  seo_keywords text,
+  seo_description text,
+
+  PRIMARY KEY (uid),
+  KEY parent (pid)
+);
+
+
+
+#
+# Table structure for table 'tx_route_marker_mm_tx_route_category'
+# 
+CREATE TABLE tx_route_marker_mm_tx_route_category (
+  uid_local int(11) DEFAULT '0' NOT NULL,
+  uid_foreign int(11) DEFAULT '0' NOT NULL,
+  tablenames varchar(30) DEFAULT '' NOT NULL,
+  sorting int(11) DEFAULT '0' NOT NULL,
+  sorting_foreign int(11) unsigned DEFAULT '0' NOT NULL,
+  KEY uid_local (uid_local),
+  KEY uid_foreign (uid_foreign)
 );
 
 
@@ -132,84 +210,6 @@ CREATE TABLE tx_route_path_mm_tx_route_category (
 # Table structure for table 'tx_route_path_mm_tx_route_marker'
 # 
 CREATE TABLE tx_route_path_mm_tx_route_marker (
-  uid_local int(11) DEFAULT '0' NOT NULL,
-  uid_foreign int(11) DEFAULT '0' NOT NULL,
-  tablenames varchar(30) DEFAULT '' NOT NULL,
-  sorting int(11) DEFAULT '0' NOT NULL,
-  sorting_foreign int(11) unsigned DEFAULT '0' NOT NULL,
-  KEY uid_local (uid_local),
-  KEY uid_foreign (uid_foreign)
-);
-
-
-
-#
-# Table structure for table 'tx_route_marker'
-#
-CREATE TABLE tx_route_marker (
-  uid int(11) NOT NULL auto_increment,
-  pid int(11) DEFAULT '0' NOT NULL,
-  tstamp int(11) DEFAULT '0' NOT NULL,
-  crdate int(11) DEFAULT '0' NOT NULL,
-  cruser_id int(11) DEFAULT '0' NOT NULL,
-  deleted tinyint(4) DEFAULT '0' NOT NULL,
-
-  sys_language_uid int(11) DEFAULT '0' NOT NULL,
-  l10n_parent int(11) DEFAULT '0' NOT NULL,
-  l10n_diffsource mediumtext,
-
-  title tinytext,
-  tx_route_category tinytext,
-  short tinytext,
-  bodytext mediumtext NOT NULL,
-
-  lat text NOT NULL,
-  lon text NOT NULL,
-  address text NOT NULL,
-  url text NOT NULL,
-
-  tx_route_path tinytext,
-
-  list_title tinytext,
-  list_short mediumtext,
-  map_title tinytext,
-  map_short mediumtext,
-
-  image text,
-  imagecaption text,
-  imageseo text,
-  imageheight tinytext,
-  imagewidth tinytext,
-  imageorient tinyint(4) unsigned NOT NULL default '0',
-  imagecaption text,
-  imagecols tinyint(4) unsigned NOT NULL default '0',
-  imageborder tinyint(4) unsigned NOT NULL default '0',
-  imagecaption_position varchar(12) default '',
-  image_link text,
-  image_zoom tinyint(3) unsigned NOT NULL default '0',
-  image_noRows tinyint(3) unsigned NOT NULL default '0',
-  image_effects tinyint(3) unsigned NOT NULL default '0',
-  image_compression tinyint(3) unsigned NOT NULL default '0',
-  image_frames tinyint(3) unsigned NOT NULL default '0',
-
-  hidden tinyint(4) DEFAULT '0' NOT NULL,
-  starttime int(11) DEFAULT '0' NOT NULL,
-  endtime int(11) DEFAULT '0' NOT NULL,
-  fe_group varchar(100) DEFAULT '0' NOT NULL,
-  
-  seo_keywords text,
-  seo_description text,
-
-  PRIMARY KEY (uid),
-  KEY parent (pid)
-);
-
-
-
-#
-# Table structure for table 'tx_route_marker_mm_tx_route_category'
-# 
-CREATE TABLE tx_route_marker_mm_tx_route_category (
   uid_local int(11) DEFAULT '0' NOT NULL,
   uid_foreign int(11) DEFAULT '0' NOT NULL,
   tablenames varchar(30) DEFAULT '' NOT NULL,
