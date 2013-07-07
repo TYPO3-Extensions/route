5cb40dbc2fe74a2bc69efc4e5592f5d6b899b38e
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-07-07 16:14:05 +0000

    0.0.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/route/trunk@77365 735d13b6-9817-0410-8766-e36946ffe9aa

--- tca.php
+++ tca.php
@@ -450,7 +450,7 @@ $TCA['tx_route_path'] = array (
         'size'                => 20,
         'minitems'            => 1,
         'maxitems'            => 1,
-        'trueMaxItems'        => 1,
+        'trueMinItems'        => 1,
         'MM'                  => 'tx_route_path_mm_tx_route_category',
         'foreign_table'       => 'tx_route_category',
         'foreign_table_where' => 'AND tx_route_category.pid=###CURRENT_PID### AND tx_route_category.deleted = 0 AND tx_route_category.hidden = 0 ORDER BY tx_route_category.title',
