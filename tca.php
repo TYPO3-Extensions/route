50180e47dc9786a48e310f6e8683a3a5e8f66a7d
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-07-07 16:57:26 +0000

    0.0.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/route/trunk@77376 735d13b6-9817-0410-8766-e36946ffe9aa

--- tca.php
+++ tca.php
@@ -447,14 +447,14 @@ $TCA['tx_route_path'] = array (
       'label'     => 'LLL:EXT:route/locallang_db.xml:tx_route_path.tx_route_category',
       'config'    => array (
         'type'                => 'select',
-        'size'                => 1,
+        'size'                => 10,
         'minitems'            => 1,
         'maxitems'            => 1,
         'eval'                => 'required',
         'MM'                  => 'tx_route_path_mm_tx_route_category',
         'foreign_table'       => 'tx_route_category',
         'foreign_table_where' => 'AND tx_route_category.pid=###CURRENT_PID### AND tx_route_category.deleted = 0 AND tx_route_category.hidden = 0 ORDER BY tx_route_category.title',
-        'XXX_items' => array (
+        'items' => array (
           '0' => array (
             '0' => '',
             '1' => '',
