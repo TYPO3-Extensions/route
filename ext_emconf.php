25d616471060a2ee08ee8e2f81f83dd576cad7fe
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-09-04 23:00:46 +0000

    1.1.1 +++#i0003
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/route/trunk@79553 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = array(
                           'You can categorise and filter both: routes and POI.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '1.1.0',
+	'version' => '1.1.1',
 	'dependencies' => 'browser',
 	'conflicts' => '',
 	'priority' => '',
